ba921aa7c68f1cc53bb6867202b4435e40e9c8b6
Thiago de Oliveira Alves <towo497@gmail.com>
2023-12-19 00:59:56 -0300

    gerador-exercicios passa a trabalhar com tópicos para cadastro e seleção de questões

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,30 +1,61 @@
+const pCadastradas = document.querySelector("#pCadastradas");
+const btCarregar = document.querySelector("#btCarregar");
+const selTopicos = document.querySelector("#selTopicos");
+const opcoes = document.querySelectorAll("option");
 const txtQestao = document.querySelector("#txtQuestao");
 const txtResolucao = document.querySelector("#txtResolucao");
 const txtResposta = document.querySelector("#txtResposta");
 const btCadastrar = document.querySelector("#btCadastrar");
 const btSalvar = document.querySelector("#btSalvar");
 
-const dados = localStorage.getItem("questoes-cadastradas");
-let questoes;
+let questoes = {};
 
-if (dados != null) {
-    questoes = JSON.parse(dados);
-} else {
-    questoes = [];
-}
-
+btCarregar.addEventListener("change", carregarQuestoes, false);
 btCadastrar.addEventListener("click", cadastrarQuestao, false);
 btSalvar.addEventListener("click", salvarQuestoesComoJSON, false);
 
+// incluir nas option spans para registrar contagem de cadastradas
+for (let i = 1; i < opcoes.length; i++) {
+    const span = document.createElement("span");
+    span.setAttribute("id", opcoes[i].value);
+    opcoes[i].appendChild(span);
+}
+
+function carregarQuestoes(e) {
+    const arquivo = e.target.files[0];
+
+    const leitor = new FileReader();
+    leitor.readAsText(arquivo);
+
+    leitor.onload = function () {
+        const conteudo = leitor.result;
+        questoes = JSON.parse(conteudo);
+        opcoes[0].selected = true; // seleciona o tópico nenhum automaticamente       
+
+        contarQuestoesCadastradas();
+    }
+}
+
 function cadastrarQuestao() {
+    if (opcoes[0].selected) {
+        alert("Escolha um tópico para cadastrar a questão.");
+        return;
+    }
+
     const texto = txtQestao.value;
     const resolucao = txtResolucao.value;
     const resposta = txtResposta.value;
 
-    const questao = {texto, resolucao, resposta};
+    const questao = { texto, resolucao, resposta };
 
-    questoes.push(questao);
-    localStorage.setItem("questoes-cadastradas", JSON.stringify(questoes));
+    const topico = opcoes[selTopicos.selectedIndex].value;
+
+    if (questoes[topico]) {
+        questoes[topico].push(questao);
+    } else {
+        questoes[topico] = [];
+        questoes[topico].push(questao);
+    }
 
     txtQestao.value = "";
     txtResolucao.value = "";
@@ -32,10 +63,23 @@ function cadastrarQuestao() {
 }
 
 function salvarQuestoesComoJSON() {
-    const arquivo = new Blob([JSON.stringify(questoes)], {type: "text/plain"});
+    const arquivo = new Blob([JSON.stringify(questoes)], { type: "text/plain" });
 
     const link = document.createElement("a");
     link.setAttribute("href", URL.createObjectURL(arquivo));
     link.setAttribute("download", "questoes.json");
     link.click();
+}
+
+function contarQuestoesCadastradas() {
+    for (let i = 1; i < opcoes.length; i++) {
+        const topico = opcoes[i].value;
+    
+        if (questoes[topico] == undefined) {
+            opcoes[i].querySelector(`#${opcoes[i].value}`).innerText = "";
+        } else {
+            opcoes[i].querySelector(`#${opcoes[i].value}`).innerText = ` (${questoes[topico].length})`;
+        }
+
+    }
 }
--- js/selecao.js
+++ js/selecao.js
@@ -1,8 +1,19 @@
 const btCarregar = document.querySelector("#btCarregar");
-const container = document.querySelector("#tudo");
+const selTopicos = document.querySelector("#selTopicos");
+const opcoes = document.querySelectorAll("option");
+const dvCards = document.querySelector("#dvCards");
 let questoes;
+let topico;
 
 btCarregar.addEventListener("change", carregarQuestoes, false);
+selTopicos.addEventListener("change", construirCard, false);
+
+// incluir nas option spans para registrar contagem de selecionadas
+for (let i = 1; i < opcoes.length; i++) {
+    const span = document.createElement("span");
+    span.setAttribute("id", opcoes[i].value);
+    opcoes[i].appendChild(span);
+}
 
 function carregarQuestoes(e) {
     const arquivo = e.target.files[0];
@@ -13,40 +24,107 @@ function carregarQuestoes(e) {
     leitor.onload = function() {
         const conteudo = leitor.result;
         questoes = JSON.parse(conteudo);
-        
-        for (let i = 0; i < questoes.length; i++) {
-            construirCard(questoes[i], i);
-        }
+        opcoes[0].selected = true; // seleciona o tópico nenhum automaticamente
+        dvCards.innerHTML = "";      
 
-        criarBotaoDeExportacao();
+        contarQuestoesSelecionadas();     
     }
 }
 
-function construirCard(questao, id) {
-    const card = document.createElement("div");
-    card.setAttribute("id", "questao" + id);
-    card.setAttribute("class", "card");
-    card.style.cursor = "pointer";
-    card.style.userSelect = "none";
-    card.innerHTML = `<strong>Questão ${id + 1}:</strong>
-                      <br>
-                      ${questao.texto}
-                      <br>
-                      <br>
-                      <strong>Resolução:</strong>
-                      <br>
-                      ${questao.resolucao}
-                      <br>
-                      <br>
-                      <strong>Resposta:</strong>
-                      <br>
-                      ${questao.resposta}
-                      <br>
-                      <br>`;
-    
-    card.addEventListener("click", selecionarCard, false);
+function construirCard() {
+    dvCards.innerHTML = ""; // limpa os cards já carregados
 
-    container.appendChild(card);
+    topico = opcoes[selTopicos.selectedIndex].value;
+
+    // se selecionou algum tópico sem ter carregado questões
+    if (topico != "nenhum" && questoes == undefined) {
+        alert("Carregue um banco de questões cadastradas.");
+        return;
+    } else if (topico == "nenhum") { // se nenhum tópico válido foi selecionado
+        return;
+    }
+
+    // se tem questões carregadas
+    if (questoes != undefined) {
+        if (topico == "nenhum") return; // se nenhum tópico válido foi selecionado 
+
+        // se não existe o tópico ou ele existe e está vazio
+        if (questoes[topico] == undefined || questoes[topico].length == 0) {
+            return;
+        }
+    }
+
+    for (let id = 0; id < questoes[topico].length; id++) { 
+        const questao = questoes[topico][id];
+
+        const card = document.createElement("div");
+        card.setAttribute("id", "questao" + id);
+        card.setAttribute("class", "card");
+        card.style.cursor = "pointer";
+        card.style.userSelect = "none";
+        card.innerHTML = `<strong>Questão ${id + 1}:</strong>
+                          <br>
+                          ${questao.texto}
+                          <br>
+                          <br>
+                          <strong>Resolução:</strong>
+                          <br>
+                          ${questao.resolucao}
+                          <br>
+                          <br>
+                          <strong>Resposta:</strong>
+                          <br>
+                          ${questao.resposta}
+                          <br>
+                          <br>`;
+        
+        card.addEventListener("click", selecionarCard, false);
+
+        if (questoes[topico][id].selecionada) {
+            card.style.backgroundColor = "#0fc";
+        }
+    
+        dvCards.appendChild(card);
+    }
+    
+    criarBotaoDeExportacao();
+}
+
+function selecionarCard(e) {
+    const cardClicado = e.target;
+    const id = cardClicado.id.substring(7);
+    console.log(id)
+
+    if (!questoes[topico][id].selecionada) {
+        questoes[topico][id].selecionada = true;
+        cardClicado.style.backgroundColor = "#0fc";
+    } else {
+        questoes[topico][id].selecionada = false;
+        cardClicado.style.backgroundColor = "#fff";
+    }
+
+    contarQuestoesSelecionadas();
+}
+
+function contarQuestoesSelecionadas() {
+    let numSelecionadas = 0;
+
+    for (let i = 1; i < opcoes.length; i++) {
+        const topico = opcoes[i].value;
+
+        if (questoes[topico] == undefined) {
+            opcoes[i].querySelector(`#${opcoes[i].value}`).innerText = "";
+        } else {
+            for (let j = 0; j < questoes[topico].length; j++) {
+                if (questoes[topico][j].selecionada) {
+                    numSelecionadas++;
+                }        
+            }
+    
+            opcoes[i].querySelector(`#${opcoes[i].value}`).innerText = ` (${numSelecionadas}/${questoes[topico].length})`;
+            numSelecionadas = 0;
+        }
+    }
 }
 
 function criarBotaoDeExportacao() {
@@ -55,22 +133,9 @@ function criarBotaoDeExportacao() {
 
     btExportar.addEventListener("click", exportarPDF, false);
 
-    container.appendChild(btExportar);
+    dvCards.appendChild(btExportar);
 }
 
-function selecionarCard(e) {
-    const cardClicado = e.target;
-    const id = cardClicado.id.substring(7);
-    console.log(id)
-
-    if (!questoes[id].selecionada) {
-        questoes[id].selecionada = true;
-        cardClicado.style.backgroundColor = "#0fc"
-    } else {
-        questoes[id].selecionada = false;
-        cardClicado.style.backgroundColor = "#fff"
-    }
-}
 
 function exportarPDF() {
     let texto = "";
@@ -78,13 +143,13 @@ function exportarPDF() {
     let numeroDaQuestao = 1;
 
     for (let i = 0; i < questoes.length; i++) {
-        if (questoes[i].selecionada) {
-            texto += `<strong>${numeroDaQuestao++})</strong> ${questoes[i].texto}
+        if (questoes[topico][i].selecionada) {
+            texto += `<strong>${numeroDaQuestao++})</strong> ${questoes[topico][i].texto}
                       <br>
                       <br>`;
-            respostas += `<strong>${numeroDaQuestao++})</strong> ${questoes[i].resolucao}
+            respostas += `<strong>${numeroDaQuestao++})</strong> ${questoes[topico][i].resolucao}
                          <br>
-                         Resposta: ${questoes[i].resposta}
+                         Resposta: ${questoes[topico][i].resposta}
                          <br>
                          <br>`;
         }
